7291afccabe9cb7b9b2e1ad11d29c5a01663e779
Tawsif Ahmed <Sif@Tawsifs-MacBook-Pro.local>
2017-09-21 17:31:51 -0400

    added ble-manager

--- android/app/src/main/java/com/stackathon/MainApplication.java
+++ android/app/src/main/java/com/stackathon/MainApplication.java
@@ -3,6 +3,7 @@ package com.stackathon;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import it.innove.BleManagerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new BleManagerPackage(),
             new VectorIconsPackage(),
             new RNDeviceInfo()
       );
